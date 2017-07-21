d6309984d659543fc3b95455af97596cbd7133a9
Kenneth Moreland <kmorel@sandia.gov>
2017-07-21 04:15:25 +0000

    Fix some titles

--- OasisMembers/tests.py
+++ OasisMembers/tests.py
@@ -30,6 +30,7 @@ class MembersViewTests(django.test.TestCase):
         """Test the view with a list of members."""
         response = self.client.get(django.urls.reverse('OasisMembers:members'))
         self.assertContains(response, 'Colin Fry')
+        self.assertContains(response, '<title>Oasis Journeys Members</title>')
 
 class MemberViewTests(django.test.TestCase):
     fixtures = [ 'testdata.json' ]
@@ -40,6 +41,9 @@ class MemberViewTests(django.test.TestCase):
             'OasisMembers:member',
             kwargs={'member_id':1}))
         self.assertContains(response, '<h1>Sylvia Browne</h1>', html=True)
+        self.assertContains(response,
+                            '<title>Oasis Member Sylvia Browne</title>',
+                            html=True)
 
 class MeetingsViewTests(django.test.TestCase):
     fixtures = [ 'testdata.json' ]
@@ -51,6 +55,10 @@ class MeetingsViewTests(django.test.TestCase):
             response,
             '<a href="/meeting/1">2017-07-01 04:58:33+00:00 - Awakening</a>',
             html=True)
+        self.assertContains(
+            response,
+            '<title>Oasis Journeys Meetings</title>',
+            html=True)
 
 class MeetingViewTests(django.test.TestCase):
     fixtures = [ 'testdata.json' ]
@@ -61,4 +69,5 @@ class MeetingViewTests(django.test.TestCase):
             'OasisMembers:meeting',
             kwargs={'meeting_id':6}))
         self.assertContains(response, 'Meeting @ July 5, 2017')
+        self.assertContains(response, '<title>Oasis Meeting @')
 
